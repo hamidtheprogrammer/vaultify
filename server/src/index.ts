81f89f347f91fc7ccae280fa60554e6a70c96cf7
Hamid <owoyemiameed@gmail.com>
2025-03-22 17:51:46 +0000

    added cd pipeline for auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,7 +48,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from vaultify");
 });
 
-const port = Number(process.env.PORT) || 8000;
+const port = Number(process.env.PORT) || 80;
 
 app.listen(port, "0.0.0.0", () => {
   console.log("server up!");
